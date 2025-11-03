029bb036a12edfb458832f7d821adbcb1539a065
Ir. Radinal Dwiki Novendra <radinal.dwiki.novendra@gmail.com>
2025-11-03 09:49:09 +0700

    menambahkan komentar pembuat program

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+// Program ini dibuat oleh kelompok 100
+
 import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
