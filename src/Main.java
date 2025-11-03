4fa55852113cc683ad458329d684a1232fbdaa08
Ir. Radinal Dwiki Novendra <radinal.dwiki.novendra@gmail.com>
2025-11-03 09:25:13 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int menu = -1;
+
+        Set<String> listMenu = new HashSet<>();
+
+        while (menu != 0) {
+
+            System.out.println("\nMenu Aplikasi\n==========");
+            System.out.println("1. Tambah Pesanan");
+            System.out.println("2. Hapus Pesanana");
+            System.out.println("3. Tampilkan Pesanan");
+            System.out.println("4. Keluar\n");
+            System.out.print("Pilih menu : ");
+
+            menu = in.nextInt();
+            switch (menu){
+                case 1:
+                    System.out.print("\nMasukkan nama menu yang ingin ditambah: ");
+                    String namaMenu = in.next();
+                    listMenu.add(namaMenu);
+                    break;
+
+                case 2:
+                    System.out.print("\nMasukkan nama menu yang inin dihapus: ");
+                    String namaMenuDihapus = in.next();
+                    listMenu.remove(namaMenuDihapus);
+                    break;
+
+                case 3:
+                    System.out.println("\nTampilkan pesanan");
+                    System.out.println("==================");
+
+                    for (Iterator it = listMenu.iterator(); it.hasNext(); ) {
+                        System.out.println("Menu Dipesan : "+it.next());
+                    }
+
+                    break;
+
+                case 4:
+                    System.out.println("Exit");
+                    menu = 0;
+                    break;
+
+                    default:
+                        System.out.println("Menu tidak tersedia");
+                        break;
+            }
+        }
+    }
+}
